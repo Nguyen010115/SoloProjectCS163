d179cb874bb5c582223833a0911303debca072bb
Mai Trung Nguyen <maitrungnguyen010115@gmail.com>
2024-08-23 10:28:38 +0700

    Tree 234: debug sliding button.

--- render/234Tree.cpp
+++ render/234Tree.cpp
@@ -938,7 +938,11 @@ void render234Tree(Screen& currentScreen) {
 
         // Ensure slidingButton doesn't go out of bounds
         if (slidingButton.x < minX) slidingButton.x = minX;
-        if (slidingButton.x > maxX) slidingButton.x = maxX;
+        if (slidingButton.x > maxX) {
+            slidingButton.x = maxX;
+            stateIndex234 = testTree.getStepsSize() - 1;
+        }
+
 
         // Update the stateIndex based on the slidingButton's position
         if (testTree.getStepsSize() > 0) {
